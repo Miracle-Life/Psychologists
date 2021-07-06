09d614606f4524a2d4a85d8059dc11bb4f282e22
belov <belovdenis777@gmail.com>
2021-07-06 22:49:05 +0300

    starting114

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,1 +1,1 @@
-import React from "react";const App = () => {    return (            <div className='container'>                Hello            </div>    );};export default App;
+import React from "react";const App = () => {    return (            <div className='container'>                Hello            </div>    );};export default App;
