68096ae021db8b703c63cb55c97861c9db4b5b3f
Denys Bielov <belovdenys@gmail.com>
2021-10-12 23:37:54 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// BrowserRouter
-import {HashRouter as Router} from "react-router-dom";
+// BrowserRouter  HashRouter
+import {BrowserRouter as Router} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
